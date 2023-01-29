5d725c8bddbac3dcdee8e175254ca065a6b8f097
ignat82 <ignat.nushtaev@outlook.com>
2023-01-29 12:11:57 +0300

    initial

---
+++ src/main/java/com/example/randomtext/RandomTextApplication.java
@@ -0,0 +1,13 @@
+package com.example.randomtext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomTextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/randomtext/RandomTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randomtext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
